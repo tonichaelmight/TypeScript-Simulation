c228adc90ac542069e89b024de0c10332d17c3dd
Michael S <tonichaelmight@gmail.com>
2023-01-15 22:35:54 -0500

    build stats testing

---
+++ test.ts
@@ -0,0 +1,32 @@
+import { appendFileSync, writeFileSync } from 'fs';
+import { Player } from './classes/Player';
+
+const players = [
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player(),
+    new Player()
+];
+
+for (let i = 0; i < players.length; i++) {
+
+    const filePath = `test/player${i}.csv`;
+    writeFileSync(filePath, '');
+    
+    appendFileSync(filePath, 'level,health,mana,physicalAttack,physicalDefense,magicAttack,magicDefense,speed,luck,health_martix,mana_martix,physicalAttack_martix,physicalDefense_martix,magicAttack_martix,magicDefense_martix,speed_martix,luck_martix\n');
+
+    appendFileSync(filePath, `${players[i].stats.level},${players[i].stats.health},${players[i].stats.mana},${players[i].stats.physicalAttack},${players[i].stats.physicalDefense},${players[i].stats.magicAttack},${players[i].stats.magicDefense},${players[i].stats.speed},${players[i].stats.luck},${players[i].levelingMatrix.health},${players[i].levelingMatrix.mana},${players[i].levelingMatrix.physicalAttack},${players[i].levelingMatrix.physicalDefense},${players[i].levelingMatrix.magicAttack},${players[i].levelingMatrix.magicDefense},${players[i].levelingMatrix.speed},${players[i].levelingMatrix.luck}\n`);
+
+    do {
+        players[i].levelUp();
+
+        appendFileSync(filePath, `${players[i].stats.level},${players[i].stats.health},${players[i].stats.mana},${players[i].stats.physicalAttack},${players[i].stats.physicalDefense},${players[i].stats.magicAttack},${players[i].stats.magicDefense},${players[i].stats.speed},${players[i].stats.luck},${players[i].levelingMatrix.health},${players[i].levelingMatrix.mana},${players[i].levelingMatrix.physicalAttack},${players[i].levelingMatrix.physicalDefense},${players[i].levelingMatrix.magicAttack},${players[i].levelingMatrix.magicDefense},${players[i].levelingMatrix.speed},${players[i].levelingMatrix.luck}\n`);
+
+    } while (players[i].stats.level < 100); 
+}
