90fc1c9c9d2f385fcb94bc15b9d9a0481dd2fa06
Michael S <tonichaelmight@gmail.com>
2023-01-17 01:20:00 -0500

    finalize tests for stats

--- test.ts
+++ test.ts
@@ -19,7 +19,7 @@ for (let i = 0; i < players.length; i++) {
     const filePath = `test/player${i}.csv`;
     writeFileSync(filePath, '');
     
-    appendFileSync(filePath, 'level,health,mana,physicalAttack,physicalDefense,magicAttack,magicDefense,speed,luck,health_martix,mana_martix,physicalAttack_martix,physicalDefense_martix,magicAttack_martix,magicDefense_martix,speed_martix,luck_martix\n');
+    appendFileSync(filePath, 'level,health,mana,physicalAttack,physicalDefense,magicAttack,magicDefense,speed,luck,health_matrix,mana_matrix,physicalAttack_matrix,physicalDefense_matrix,magicAttack_matrix,magicDefense_matrix,speed_matrix,luck_matrix\n');
 
     appendFileSync(filePath, `${players[i].stats.level},${players[i].stats.health},${players[i].stats.mana},${players[i].stats.physicalAttack},${players[i].stats.physicalDefense},${players[i].stats.magicAttack},${players[i].stats.magicDefense},${players[i].stats.speed},${players[i].stats.luck},${players[i].levelingMatrix.health},${players[i].levelingMatrix.mana},${players[i].levelingMatrix.physicalAttack},${players[i].levelingMatrix.physicalDefense},${players[i].levelingMatrix.magicAttack},${players[i].levelingMatrix.magicDefense},${players[i].levelingMatrix.speed},${players[i].levelingMatrix.luck}\n`);
 
